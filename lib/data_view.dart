de3e36f751dadc8e8a85196a5e74ea2b0c1e6a35
Youssef_Frigui <youssef.frigui13@gmail.com>
2023-05-19 20:53:58 +0100

    19/5823 20:53

--- lib/data_view.dart
+++ lib/data_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ins_app/display_data_docs.dart';
-
+import 'package:marquee/marquee.dart';
 
 class DataView extends StatelessWidget {
   final String cin;
@@ -12,51 +12,141 @@ class DataView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Data View'),
+        backgroundColor: Color.fromARGB(255, 94, 6, 247),
+        toolbarHeight: 80,
+        centerTitle: true,
+        title: Container(
+          width: 100,
+          child: Transform.scale(
+            scale: 1,
+            child: Text(
+              'Data View',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('Citoyen')
-            .doc(cin)
-            .collection('data')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          QuerySnapshot querySnapshot = snapshot.data!;
-          List<QueryDocumentSnapshot> documents = querySnapshot.docs;
-
-          return ListView.builder(
-            padding: EdgeInsets.all(16.0),
-            itemCount: documents.length,
-            itemBuilder: (context, index) {
-              QueryDocumentSnapshot document = documents[index];
-              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-
-              return GestureDetector(
-                onTap: () {
-                  // Perform actions on tap if needed
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => DataDocDisplay(
-                      cin: cin,
-                      documentID: document.id,
-                    ),
-                  ));
-
-                },
-                child: Card(
-                  child: ListTile(
-                    title: Text(document.id),
-                  ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Expanded(
+            
+              child: Marquee(
+                startAfter: Duration(seconds: 1),
+                accelerationCurve: Curves.linear,
+                velocity: 100,
+                scrollAxis: Axis.horizontal,
+                fadingEdgeStartFraction: 0.2,
+        fadingEdgeEndFraction: 0.2,
+        blankSpace: 50,
+                text: 'Tables relatives à $cin',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
-              );
-            },
-          );
-        },
+              ),
+            ),
+          
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              height: 210,
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('Citoyen')
+                    .doc(cin)
+                    .collection('data')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  QuerySnapshot querySnapshot = snapshot.data!;
+                  List<QueryDocumentSnapshot> documents = querySnapshot.docs;
+
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: documents.map((document) {
+                        Map<String, dynamic> data =
+                            document.data() as Map<String, dynamic>;
+
+                        return Container(
+                          width: 175,
+                          margin: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                spreadRadius: 2,
+                                blurRadius: 4,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: GestureDetector(
+                            onTap: () {
+                              // Perform actions on tap if needed
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => DataDocDisplay(
+                                  cin: cin,
+                                  documentID: document.id,
+                                ),
+                              ));
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 180.0,
+                                  height: 120.0,
+                                  margin: EdgeInsets.all(12.0),
+                                  decoration: BoxDecoration(
+                                    //add image 
+                                    
+                                    color: Color.fromARGB(255, 94, 6, 247),
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  child: Icon(
+                                    Icons.money,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(12.0),
+                                  child: Text(
+                                    document.id,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
